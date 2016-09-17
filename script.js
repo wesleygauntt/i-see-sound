ea63e7c07b4ee46e4d465de2c44587a856410995
Wesley Gauntt <wesleygauntt92@gmail.com>
2016-09-17 13:47:48 -0700

    sounds: added basic sound functionality

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-var Visuals = function(){
+var Sounds = function(){
+	var self = this;
+	this.client_id = 'CLIENT_ID';
+
+	this.initialize = function(){
+		SC.initialize({
+		  client_id: self.client_id
+		});
+	}
+
+	this.play = function(url){
+		SC.get(url).then(function(sound, error) {
+			var source = sound.stream_url + '?client_id=' + self.client_id;
+  	  $('#player').attr('src', source);
+		});
+	}
+}
+
+var Sights = function(){
 	this.render = function(){
 		console.log("Hello, world!");
 	}
 }
 
-var app = new Visuals();
-app.render();
+var sight = new Sights();
+var sound = new Sounds();
+
+sound.initialize();
+sound.play('/tracks/293');
