824e43e1b2c17600ee798063f5b9d976a7a08b8c
Wesley Gauntt <wesleygauntt92@gmail.com>
2016-09-18 10:36:28 -0700

    sight: added graph rendering, sound: Soundcloud integration, styles: added bar class, index: commented Bootstrap

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+var Sights = function(){
+	var self = this;
+	this.bars = [];
+
+	this.render = function(height, frequencyData){
+		var length = self.bars.length;
+
+    for (var i = 0; i < length; i++) {
+      var bar = self.bars[i];
+      bar.height((frequencyData[i] / 250) * height + 'px');
+    }
+	}
+
+	this.prepare = function(height, width, count){
+	  var barWidth = (width / count);
+
+	  for (var i = 0; i < count; i++) {
+      var left = barWidth * i;
+      var $d = $("<div>", {class: "bar", style: `left: ${left}px; width: ${barWidth}px`});
+      self.bars.push($d);
+      $('#visualization').append($d);
+	  }
+	}
+}
+
 var Sounds = function(){
 	var self = this;
 	this.client_id = 'CLIENT_ID';
@@ -8,22 +33,45 @@ var Sounds = function(){
 		});
 	}
 
-	this.play = function(url){
+	this.loadTrack = function(url){
 		SC.get(url).then(function(sound, error) {
 			var source = sound.stream_url + '?client_id=' + self.client_id;
+  	  $('#player').attr('crossOrigin', 'anonymous');
   	  $('#player').attr('src', source);
 		});
 	}
-}
 
-var Sights = function(){
-	this.render = function(){
-		console.log("Hello, world!");
+	/*
+		https://www.patrick-wied.at/blog/how-to-create-audio-visualizations-with-javascript-html
+	*/
+	this.play = function(){
+		var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+		var audioElement = document.getElementById('player');
+		var audioSrc = audioCtx.createMediaElementSource(audioElement);
+		var analyser = audioCtx.createAnalyser();
+
+		// Bind our analyser to the media element source.
+		audioSrc.connect(analyser);
+		audioSrc.connect(audioCtx.destination);
+
+		var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+		
+		sights.prepare(200, 400, (analyser.frequencyBinCount / 2));
+		// we're ready to receive some data!
+		function renderFrame() {
+		   requestAnimationFrame(renderFrame);
+		   // update data in frequencyData
+		   analyser.getByteFrequencyData(frequencyData);
+		   // render frame based on values in frequencyData
+		   sights.render(400, frequencyData)
+		}
+		renderFrame();
 	}
 }
 
-var sight = new Sights();
-var sound = new Sounds();
 
+var sights = new Sights();
+var sound = new Sounds();
 sound.initialize();
-sound.play('/tracks/293');
+sound.loadTrack('/tracks/195549412');
+sound.play();
